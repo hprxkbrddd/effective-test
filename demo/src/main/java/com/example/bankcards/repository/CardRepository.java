f3ff234f989ce2c0bb193b63a91c1a2340f0fa71
e901st <egorushkinnikita65@gmail.com>
2025-10-05 13:19:37 +0300

    add: "find by card attributes" methods

--- demo/src/main/java/com/example/bankcards/repository/CardRepository.java
+++ demo/src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -4,6 +4,11 @@ import com.example.bankcards.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CardRepository extends JpaRepository<Card, String> {
+public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findByOwnerId(String ownerId);
+    Optional<Card> findByCardNumber(String cardNumber);
 }
--- demo/src/main/java/com/example/bankcards/service/CardService.java
+++ demo/src/main/java/com/example/bankcards/service/CardService.java
@@ -4,6 +4,7 @@ import com.example.bankcards.dto.CardDTO;
 import com.example.bankcards.dto.CardType;
 import com.example.bankcards.entity.Card;
 import com.example.bankcards.repository.CardRepository;
+import com.example.bankcards.repository.UserRepository;
 import com.example.bankcards.util.CardNumberEncryptor;
 import com.example.bankcards.util.CardNumberGenerator;
 import jakarta.persistence.EntityNotFoundException;
@@ -16,18 +17,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CardService {
     private final CardRepository cardRepository;
+    private final UserRepository userRepository;
     private final CardNumberGenerator cardNumberGenerator;
 
-    public List<CardDTO> getAll(){
-        return cardRepository.findAll()
-                .stream().map(Card::toDTO)
-                .toList();
+    public List<Card> getAll(){
+        return cardRepository.findAll();
     }
 
-    public CardDTO getById(String cardId){
+    public CardDTO getById(Long cardId){
         return cardRepository.findById(cardId)
                 .orElseThrow(() -> new EntityNotFoundException("Could not fetch card by id. Card is not in database"))
                 .toDTO();
+    }
+
+    public CardDTO getByNumber(String cardNumber){
+        return cardRepository.findByCardNumber(cardNumber)
+                .orElseThrow(() -> new EntityNotFoundException("Could not fetch card by card number. Card is not in database"))
+                .toDTO();
+    }
+
+    public List<CardDTO> getCardsOfUser(String userId){
+        if(userRepository.findById(userId).isEmpty())
+            throw new EntityNotFoundException("Could not fetch cards of user. User is not in database");
+        return cardRepository.findByOwnerId(userId)
+                .stream().map(Card::toDTO)
+                .toList();
     }
 
     public CardDTO create(String ownerId){
