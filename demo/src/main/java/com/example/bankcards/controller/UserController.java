194f9b185d04640566de5328f529e109d63833e5
e901st <egorushkinnikita65@gmail.com>
2025-10-05 13:15:43 +0300

    add: user can create cards and send block requests

--- demo/src/main/java/com/example/bankcards/controller/UserController.java
+++ demo/src/main/java/com/example/bankcards/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.example.bankcards.controller;
 
+import com.example.bankcards.dto.CardDTO;
+import com.example.bankcards.dto.CardEncryptedDTO;
 import com.example.bankcards.dto.UserCreationDTO;
 import com.example.bankcards.entity.Card;
+import com.example.bankcards.exception.UnauthorizedException;
+import com.example.bankcards.service.CardService;
+import com.example.bankcards.service.JwtService;
 import com.example.bankcards.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +21,8 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final CardService cardService;
+    private final JwtService jwtService;
 
     @GetMapping("/card")
     public ResponseEntity<List<Card>> getUsersCards(
@@ -24,16 +31,24 @@ public class UserController {
     }
 
     @PostMapping("/card")
-    public ResponseEntity<List<Card>> createCard(
+    public ResponseEntity<CardDTO> createCard(
             @RequestHeader("Authorization") String authHeader){
-        return null;
+        if (authHeader !=null && authHeader.startsWith("Bearer ")){
+            return ResponseEntity.ok(cardService.create(
+                    jwtService.extractId(authHeader.substring(7))
+            ));
+        }else {
+            throw new UnauthorizedException("Auth header is either not present or unreadable");
+        }
     }
 
     @PutMapping("/card/block")
     public ResponseEntity<String> blockCardRequest(
             @RequestHeader("Authorization") String authHeader,
             @RequestParam String cardId){
-        return null;
+        return ResponseEntity.ok("Block request from user-id:"+
+                jwtService.extractId(authHeader.substring(7))+
+                " has been sent to admin.\n Card to block: "+cardId);
     }
 
     @GetMapping("/card/balance")
